543a5f729a7874a31066d39b1efac371aee2d649
mksjohari <mksjohari@gmail.com>
2020-09-28 21:30:22 +1000

    fixed conflicts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Switch, useLocation } from 'react-router-dom';
 import Layout from './shared/layout';
-import Home from './pages/home/home';
 import explore from './pages/explore/explore';
 import projects from './pages/projects/projects';
 import profile from './pages/profile/profile';
+import signUp from './pages/home/signUp';
 import tempUser from './pages/profile/tempUser';
-import './styles/index.scss';
 import signIn from './shared/signIn';
+import './styles/index.scss';
 require('typeface-poppins');
 
 const Index = () => {
@@ -15,8 +15,7 @@ const Index = () => {
 	return (
 		<Layout page={location.pathname}>
 			<Switch>
-				<Route component={Home} exact path="/" />
-				<Route component={explore} path="/explore" />
+				<Route component={explore} path="/" />
 				<Route component={signUp} path="/signUp" />
 				<Route component={projects} path="/projects" />
 				<Route component={profile} path="/profile" />
