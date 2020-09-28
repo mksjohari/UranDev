5eb2dacbb920953fc8d8fb785d6fdb350a26a80b
mksjohari <mksjohari@gmail.com>
2020-09-28 21:17:11 +1000

    basic routing structure

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,68 +1,65 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 // import * as mysql from 'promise-mysql';
 
-import "reflect-metadata";
-import { createConnection } from "typeorm";
-import { User, UserType } from "./entity/user";
+import 'reflect-metadata';
+import { Connection, createConnection } from 'typeorm';
+import { User, UserType } from './entity/user';
 
 admin.initializeApp();
 
-// const connect = async (config: object) => {
-// 	// Establish a connection to the database
-// 	return await mysql.createPool({
-// 		host: functions.config().cloudsql.host,
-// 		port: functions.config().cloudsql.port,
-// 		user: functions.config().cloudsql.user, // e.g. 'my-db-user'
-// 		password: functions.config().cloudsql.pass, // e.g. 'my-db-password'
-// 		database: functions.config().cloudsql.database, // e.g. 'my-database'
-// 		...config,
-// 	});
-// };
-
 const connect = async () => {
-    return await createConnection({
-        type: "mysql",
-        host: functions.config().cloudsql.host,
-        port: functions.config().cloudsql.port,
-        username: functions.config().cloudsql.user,
-        password: functions.config().cloudsql.pass,
-        database: functions.config().cloudsql.database,
-        entities: [User],
-        synchronize: true,
-    });
+	return await createConnection({
+		type: 'mysql',
+		host: functions.config().cloudsql.host,
+		port: functions.config().cloudsql.port,
+		username: functions.config().cloudsql.user,
+		password: functions.config().cloudsql.pass,
+		database: functions.config().cloudsql.database,
+		entities: [User],
+		synchronize: true,
+	});
 };
+let connection: Connection;
 
 export const testCall = functions
-    .region("australia-southeast1")
-    .https.onCall(async (data, context) => {
-        try {
-            console.log(data);
-            console.log(context);
-            const connection = await connect();
-            const user = new User();
-            user.uid = "KHAIRIJOHARIISHERE10";
-            user.firstName = "Khairi";
-            user.lastName = "Johari";
-            user.email = "Uran@Uran.com";
-            user.photoUrl =
-                "https://scontent.fmel5-1.fna.fbcdn.net/v/t1.0-1/cp0/p60x60/117968907_3230294453683475_1839075413000905669_n.jpg?_nc_cat=109&_nc_sid=7206a8&_nc_ohc=NJee0EYZINkAX_0DXuS&_nc_ht=scontent.fmel5-1.fna&_nc_tp=27&oh=4d63d0b1c0dfc32149d67cf1b2e3465c&oe=5F90595A";
-            user.userType = UserType.MANAGER;
-
-            return connection.manager.save(user).then((user) => {
-                console.log("user has been saved. user id is", user.uid);
-            });
-        } catch (err) {
-            console.log(err);
-        }
-    });
+	.region('australia-southeast1')
+	.https.onCall(async (data, context) => {
+		try {
+			const user = new User();
+			if (!connection || !connection.isConnected) {
+				connection = await connect();
+			}
+			user.uid = '2';
+			user.firstName = 'Khairi';
+			user.lastName = 'Johari';
+			user.email = 'Uran@Uran.com';
+			user.photoUrl =
+				'https://scontent.fmel5-1.fna.fbcdn.net/v/t1.0-1/cp0/p60x60/117968907_3230294453683475_1839075413000905669_n.jpg?_nc_cat=109&_nc_sid=7206a8&_nc_ohc=NJee0EYZINkAX_0DXuS&_nc_ht=scontent.fmel5-1.fna&_nc_tp=27&oh=4d63d0b1c0dfc32149d67cf1b2e3465c&oe=5F90595A';
+			user.userType = UserType.MANAGER;
+			await connection.manager.save(user);
+			console.log('user has been saved. user id is', user.uid);
+			return `Successfully added ${user.uid}`;
+		} catch (err) {
+			console.log(err);
+			return `Error occurred adding user`;
+		}
+	});
 
 export const getUser = functions
-    .region("australia-southeast1")
-    .https.onCall(async (data, context) => {
-        console.log(context);
-        console.log("data", data);
-        const uid = data.uid;
-        const connection = await connect();
-        return connection.query(`SELECT * FROM USER WHERE UID=${uid}`);
-    });
+	.region('australia-southeast1')
+	.https.onCall(async (data, context) => {
+		console.log('data', data);
+		try {
+			const uid = data.uid;
+			if (!connection || !connection.isConnected) {
+				connection = await connect();
+			}
+			const result = connection.query(
+				`SELECT * FROM user WHERE UID=${uid};`
+			);
+			return result;
+		} catch (err) {
+			console.log(err);
+		}
+	});
--- src/shared/firebase.js
+++ src/shared/firebase.js
@@ -1,37 +1,39 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCb3O3mwrZnycpDs8sv7XJKbPE0gvRsqD4",
-    authDomain: "uran-28-12-98.firebaseapp.com",
-    databaseURL: "https://uran-28-12-98.firebaseio.com",
-    projectId: "uran-28-12-98",
-    storageBucket: "uran-28-12-98.appspot.com",
-    messagingSenderId: "113317665845",
-    appId: "1:113317665845:web:9b4e3065a9a84b7cd5a57c",
-    measurementId: "G-DDKX9CHS1L",
+	apiKey: 'AIzaSyCb3O3mwrZnycpDs8sv7XJKbPE0gvRsqD4',
+	authDomain: 'uran-28-12-98.firebaseapp.com',
+	databaseURL: 'https://uran-28-12-98.firebaseio.com',
+	projectId: 'uran-28-12-98',
+	storageBucket: 'uran-28-12-98.appspot.com',
+	messagingSenderId: '113317665845',
+	appId: '1:113317665845:web:9b4e3065a9a84b7cd5a57c',
+	measurementId: 'G-DDKX9CHS1L',
 };
 
 let firebaseInstance;
 let functionsInstance;
 export const getFirebase = (firebase) => {
-    if (firebaseInstance) {
-        return firebaseInstance;
-    }
+	if (firebaseInstance) {
+		return firebaseInstance;
+	}
 
-    firebase.initializeApp(firebaseConfig);
-    firebaseInstance = firebase;
+	firebase.initializeApp(firebaseConfig);
+	firebase.functions().useFunctionsEmulator('http://localhost:5001');
+	firebaseInstance = firebase;
 
-    return firebase;
+	return firebase;
 };
 export const getFunctions = (firebase) => {
-    if (functionsInstance) {
-        return functionsInstance;
-    }
-    if (firebaseInstance) {
-        functionsInstance = firebase.app().functions("localhost:5001");
-        return functionsInstance;
-    } else {
-        firebase.initializeApp(firebaseConfig);
-        firebaseInstance = firebase;
-        functionsInstance = firebase.app().functions("localhost:5001");
-        return functionsInstance;
-    }
+	if (functionsInstance) {
+		return functionsInstance;
+	}
+	if (firebaseInstance) {
+		functionsInstance = firebaseInstance.app().functions();
+		return functionsInstance;
+	} else {
+		firebase.initializeApp(firebaseConfig);
+		firebase.functions().useFunctionsEmulator('http://localhost:5001');
+		firebaseInstance = firebase;
+		functionsInstance = firebase.app().functions();
+		return functionsInstance;
+	}
 };
