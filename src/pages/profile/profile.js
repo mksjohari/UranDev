8968b10077b6e2b3b75326f3400d7ccbd462f4a9
michelle-ew <medhie@student.unimelb.edu.au>
2020-09-28 21:35:31 +1000

    profile changes

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
-import BubbleChart from "./bubbleChart";
+import WordBubble from "./bubbleChart";
 import Button from "../../shared/Button";
+import ProfileDetails from "./profileDetails";
 
 const profile = () => {
     return (
-        <div>
-            <h1>Hello,</h1>
-            <p>I'm profile</p>
-            {/* <BubbleChart data={data} /> */}
+        <div className="parent">
+            {/* <WordBubble /> */}
+            < ProfileDetails/>
             <Button
                 colour="yellow"
                 iconR={<i className="fas fa-check"/>}
@@ -18,20 +18,3 @@ const profile = () => {
 };
 
 export default profile;
-
-var data = [
-    {
-        _id: "bubble1", // unique id (required)
-        value: 50, // used to determine relative size of bubbles (required)
-        displayText: "Number Theory", // will use _id if undefined
-        colorValue: 0, // used to determine color
-        selected: true, // if true will use selectedColor/selectedTextColor for circle/text
-    },
-    {
-        _id: "bubble2", // unique id (required)
-        value: 5, // used to determine relative size of bubbles (required)
-        displayText: "HTML", // will use _id if undefined
-        colorValue: 3, // used to determine color
-        selected: true, // if true will use selectedColor/selectedTextColor for circle/text
-    },
-];
