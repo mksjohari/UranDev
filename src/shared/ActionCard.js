2414d95c5c19484152c3bfb7d92e714cf07b357a
cheang-haoyuan <cheangh1@student.unimelb.edu.au>
2020-10-06 22:16:45 +1100

    indentation stuff

--- src/shared/ActionCard.js
+++ src/shared/ActionCard.js
@@ -6,37 +6,37 @@ import Dropdown from "./sandbox/Dropdown";
 import styles from "../modules/tmp.module.scss";
 
 class ActionCard extends React.Component {
-    render() {
-        return (
-            <Draggable 
-                draggableId={this.props.task.id} 
-                index={this.props.index}
-            >
-                {(provided) => (
-                    <div 
-                        {...provided.draggableProps}
-                        ref={provided.innerRef}
-                        className={styles.actionsCard}
-                    >
-                        <span className={styles.actionsContent}>
-                            <div className={styles.tools} >
-                                Used
-                                <Dropdown 
-                                    options={Options} 
-                                    isMulti={true} 
-                                    isCreatable={true} 
-                                    keyColour="#AFDEE9"
-                                />
-                            </div>
-                        </span>
-                        <span {...provided.dragHandleProps} className={styles.dragHandle}>
-                            <i class="fas fa-grip-lines"></i>
-                        </span>
-                    </div>
-                )}
-            </Draggable>
-        );
-    }
+  render() {
+    return (
+      <Draggable 
+        draggableId={this.props.task.id} 
+        index={this.props.index}
+      >
+        {(provided) => (
+          <div 
+            {...provided.draggableProps}
+            ref={provided.innerRef}
+            className={styles.actionsCard}
+          >
+            <span className={styles.actionsContent}>
+              <div className={styles.tools} >
+                Used
+                <Dropdown 
+                  options={Options} 
+                  isMulti={true} 
+                  isCreatable={true} 
+                  keyColour="#AFDEE9"
+                />
+              </div>
+            </span>
+            <span {...provided.dragHandleProps} className={styles.dragHandle}>
+              <i class="fas fa-grip-lines"></i>
+            </span>
+          </div>
+        )}
+      </Draggable>
+    );
+  }
 }
 
 export default ActionCard;
--- src/shared/TaskCol.js
+++ src/shared/TaskCol.js
@@ -8,142 +8,142 @@ import styles from "../modules/tmp.module.scss";
 // selective rendering to save computation timee
 class InnerList extends PureComponent {
 
-    render () {
-        return (
-            this.props.tasks.map((task, index) => (
-                <ActionCard key={task.id} task={task} index={index}/>
-            )) 
-        )
-    }
+  render () {
+  return (
+    this.props.tasks.map((task, index) => (
+    <ActionCard key={task.id} task={task} index={index}/>
+    )) 
+  )
+  }
 
 }
 
 // add button?
 class AddActionBtn extends Component {
-    render () {
-        return (
-            <button className={styles.addBtn}>
-                <i class="fas fa-plus"></i>
-            </button>
-        )
-    }
+  render () {
+    return (
+    <button className={styles.addBtn}>
+      <i class="fas fa-plus"></i>
+    </button>
+    )
+  }
 }
 
 // add action
 function AddAction(key, task, index) {
-    return;
+  return;
 }
 
 class TaskCol extends Component {
-    state = {taskTitle: "", taskDesc: ""};
+  state = {taskTitle: "", taskDesc: ""};
 
-    handleChange(event) {
+  handleChange(event) {
 
-        if (event.target.className === styles.taskTitle) {
-            var newTitle = event.target.value.replace(/\n/g, "");
-            event.target.value = newTitle;
+    if (event.target.className === styles.taskTitle) {
+    var newTitle = event.target.value.replace(/\n/g, "");
+    event.target.value = newTitle;
 
-            const newState = {
-                ...this.state,
-                taskTitle: newTitle,
-            };
+    const newState = {
+      ...this.state,
+      taskTitle: newTitle,
+    };
 
-            this.setState(newState);
-            
-            return;
-        }
-
-        // don't allow too many new lines
-        var newEntry = event.target.value.replace(/\n\n/g, '\n');
-        event.target.value = newEntry;
-
-        const newState = {
-            ...this.state,
-            taskDesc: newEntry,
-        };
-
-        this.setState(newState);
-
+    this.setState(newState);
+    
+    return;
     }
 
-    resetHeight(event) {
-        event.target.style.height = 1 + "px";
-        event.target.style.height = (5 + event.target.scrollHeight) + "px";
-        // console.log(event.target.scrollHeight);
+    // don't allow too many new lines
+    var newEntry = event.target.value.replace(/\n\n/g, '\n');
+    event.target.value = newEntry;
+
+    const newState = {
+    ...this.state,
+    taskDesc: newEntry,
+    };
+
+    this.setState(newState);
+
+  }
+
+  resetHeight(event) {
+    event.target.style.height = 1 + "px";
+    event.target.style.height = (5 + event.target.scrollHeight) + "px";
+    // console.log(event.target.scrollHeight);
+  }
+
+  // bind 'this'
+  handleChange = this.handleChange.bind(this);
+  resetHeight = this.resetHeight.bind(this);
+
+  dropHeight(event) {
+    if (event.target.style.height < 300) {
+    return true;
     }
+    return false;
+  }
 
-    // bind 'this'
-    handleChange = this.handleChange.bind(this);
-    resetHeight = this.resetHeight.bind(this);
+  render() {
 
-    dropHeight(event) {
-        if (event.target.style.height < 300) {
-            return true;
-        }
-        return false;
-    }
+    return (
+    <Draggable draggableId={this.props.col.id} index={this.props.index}>
+      {(provided) => (
+        <div 
+        className={styles.taskCol}
+        {...provided.draggableProps}
+        ref={provided.innerRef}
+        >
+        {/* <div {...provided.dragHandleProps}>{this.props.col.title}</div> */}
+        <div className={styles.taskHeader}>
 
-    render() {
+          <textarea
+            placeholder="Click to edit task title"
+            value={this.state.value}
+            onKeyUp={this.resetHeight}
+            onChange={this.handleChange} 
+            className={styles.taskTitle}
+          />
+          <span className={styles.dragHandle} {...provided.dragHandleProps}>
+            <i class="fas fa-grip-lines-vertical"></i>
+          </span>
 
-        return (
-            <Draggable draggableId={this.props.col.id} index={this.props.index}>
-                {(provided) => (
-                    <div 
-                        className={styles.taskCol}
-                        {...provided.draggableProps}
-                        ref={provided.innerRef}
-                    >
-                        {/* <div {...provided.dragHandleProps}>{this.props.col.title}</div> */}
-                        <div className={styles.taskHeader}>
+        </div>
+        <div className={styles.taskInfo}>
+          <textarea
+            placeholder="description"
+            value={this.state.value}
+            onKeyUp={this.resetHeight}
+            onChange={this.handleChange} 
+            className={styles.taskDesc}
+          />
+          <Button 
+            color="white" 
+                text="Task Duration" 
+                iconL={<i class="fas fa-calendar"></i>} 
+                className={styles.taskDuration}
+              />
+            </div>
 
-                            <textarea
-                                placeholder="Click to edit task title"
-                                value={this.state.value}
-                                onKeyUp={this.resetHeight}
-                                onChange={this.handleChange} 
-                                className={styles.taskTitle}
-                            />
-                            <span className={styles.dragHandle} {...provided.dragHandleProps}>
-                                <i class="fas fa-grip-lines-vertical"></i>
-                            </span>
-
-                        </div>
-                        <div className={styles.taskInfo}>
-                            <textarea
-                                placeholder="description"
-                                value={this.state.value}
-                                onKeyUp={this.resetHeight}
-                                onChange={this.handleChange} 
-                                className={styles.taskDesc}
-                            />
-                            <Button 
-                                color="white" 
-                                text="Task Duration" 
-                                iconL={<i class="fas fa-calendar"></i>} 
-                                className={styles.taskDuration}
-                            />
-                        </div>
-
-                        <Droppable droppableId={this.props.col.id} type="actions">
-                            {(provided, snapshot) => (
-                                <div 
-                                    ref={provided.innerRef}
-                                    {...provided.droppableProps}
-                                    className={styles.dropArea}
-                                >
-                                    <div className={snapshot.isDraggingOver ? styles.draggingOver: ''}>
-                                        <InnerList tasks={this.props.tasks} />
-                                        {provided.placeholder}
-                                    </div>
-                                    <AddActionBtn />
-                                </div>
-                            )}
-                        </Droppable>
-                    </div>
-                )}
-            </Draggable>
-        );
-    }
+            <Droppable droppableId={this.props.col.id} type="actions">
+              {(provided, snapshot) => (
+                <div 
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                  className={styles.dropArea}
+                >
+                  <div className={snapshot.isDraggingOver ? styles.draggingOver: ''}>
+                    <InnerList tasks={this.props.tasks} />
+                    {provided.placeholder}
+                  </div>
+                  <AddActionBtn />
+                </div>
+              )}
+            </Droppable>
+          </div>
+        )}
+      </Draggable>
+    );
+  }
 }
 
 export default TaskCol;
