a6803dff4c35d36d422db8aa8aa2674571487fd8
cheang-haoyuan <cheangh1@student.unimelb.edu.au>
2020-10-09 17:55:58 +1100

    datepicker file

--- src/pages/tmp/tmpTest.js
+++ src/pages/tmp/tmpTest.js
@@ -1,10 +1,12 @@
 import React from "react";
 import DnD from "../../shared/DnD";
+import DateSelect from "../../shared/DateSelect";
 import Data from "../../shared/sampleData"
 
 const TmpTest = () => {
   return (
     <div className="tmp">
+      <DateSelect />
       <DnD 
         actions={Data.actions}
         tasks={Data.tasks}
