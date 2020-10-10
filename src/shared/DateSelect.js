7ee94003c7ddc6e88c06a1d18bb75c35e19daece
cheang-haoyuan <cheangh1@student.unimelb.edu.au>
2020-10-10 20:28:48 +1100

    react dates styling

--- src/shared/DateSelect.js
+++ src/shared/DateSelect.js
@@ -1,23 +1,55 @@
 import React from 'react';
+//import ThemedStyleSheet from 'react-with-styles/lib/ThemedStyleSheet';
+//import aphroditeInterface from 'react-with-styles-interface-aphrodite';
+//import DefaultTheme from 'react-dates/lib/theme/DefaultTheme';
+
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
+import '../modules/react_dates_overrides.css'
 import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+import styles from '../modules/react_dates_overrides.module.scss';
+
+// ThemedStyleSheet.registerInterface(aphroditeInterface);
+// ThemedStyleSheet.registerTheme({
+//   reactDates: {
+//     ...DefaultTheme.reactDates,
+//     color: {
+//       ...DefaultTheme.reactDates.color,
+//       highlighted: {
+//         backgroundColor: 'lightblue',
+//         backgroundColor_active: 'lightblue',
+//         backgroundColor_hover: 'lightblue',
+//         color: 'lightblue',
+//         color_active: 'lightblue',
+//         color_hover: 'lightblue',
+//       },
+//     },
+//   },
+// });
 
 class DateSelect extends React.Component {
-  state = {startDate: '1 January 2020', endDate: '1 February 2020'};
+  state = {};
+  
   render() {
     return(
-      <>
-      <DateRangePicker
-        startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-        startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
-        endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-        endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-        onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
-        focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-        onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-      />
-      </>
+      <div className={styles.popUpContainer}>
+        <DateRangePicker
+          startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+          startDateId={this.props.taskId + "_startDate"} // PropTypes.string.isRequired,
+          endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+          endDateId={this.props.taskId + "_endDate"} // PropTypes.string.isRequired,
+          onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+          focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+          onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+          disableScroll={true}
+          showClearDates
+          reopenPickerOnClearDates
+          customInputIcon={<i className={`fas fa-calendar`} ></i>}
+          small
+          minimumNights={0}
+          isOutsideRange={() => false}
+        />
+      </div>
     )
   }
 }
