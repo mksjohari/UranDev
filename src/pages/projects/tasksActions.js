bc2e94bd6fc19a118b3a4fa0ba038d4e6e6e650e
mksjohari <mksjohari@gmail.com>
2020-10-14 00:44:16 +1100

    alphabeta

--- src/pages/projects/tasksActions.js
+++ src/pages/projects/tasksActions.js
@@ -1,69 +1,68 @@
-import React from "react";
-import { Formik, Field } from "formik";
+import React from 'react';
+import { Formik, Field } from 'formik';
 
-import TaskDnD from "../../shared/reactDnD/taskDnD";
-import {taskData} from "../tmp/tmpTest";
-import Button from "../../shared/sandbox/Button";
-import { withContext } from "../../shared/react-dims";
-import styles from "../../modules/createProject.module.scss";
+import TaskDnD from '../../shared/reactDnD/taskDnD';
+import { taskData } from '../tmp/tmpTest';
+import Button from '../../shared/sandbox/Button';
+import { withContext } from '../../shared/react-dims';
+import styles from '../../modules/createProject.module.scss';
 
 function tasksActions(props) {
-    console.log(props);
-    return (
-        <Formik
-            initialValues={{
-                situation: props.form.situation,
-                role: props.form.role,
-                teamSize: props.form.teamSize,
-                budget: props.form.budget,
-                currency: props.form.currency,
-            }}
-            // onSubmit={async (values) => {
-            //     await new Promise((r) => setTimeout(r, 500));
-            //     alert(JSON.stringify(values, null, 2));
-            //     props.setProject(values);
-            //     props.nextStep();
-            // }}
-            onSubmit={(values, actions) => {
-                setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
-                    props.setProject(values);
-                    props.nextStep();
-                    actions.setSubmitting(false);
-                }, 1000);
-            }}
-        >
-            {(props) => (
-                <form onSubmit={props.handleSubmit}>
-                    <div className={styles.section_input}>
-                        <label
-                            htmlFor="situation"
-                            className={styles.section_question}
-                        >
-                            What is the problem space/goal of your project?
-                            {` `}
-                            What inspired you to start this project?
-                            {` `}
-                            Give a brief summary of your project's background.
-                        </label>
-                        <TaskDnD data={taskData}/>
-                        <Field
-                            as="textarea"
-                            className={`inp-field ${styles.input_situation}`}
-                            name="situation"
-                            placeholder="Type here"
-                        />
-                    </div>
-                    <Button
-                        type="submit"
-                        className={styles.save_draft}
-                        iconR={<i className="fas fa-arrow-right" />}
-                        text="Next"
-                        onClick={props.submitForm}
-                    />
-                </form>
-            )}
-        </Formik>
-    );
+	return (
+		<Formik
+			initialValues={{
+				situation: props.form.situation,
+				role: props.form.role,
+				teamSize: props.form.teamSize,
+				budget: props.form.budget,
+				currency: props.form.currency,
+			}}
+			// onSubmit={async (values) => {
+			//     await new Promise((r) => setTimeout(r, 500));
+			//     alert(JSON.stringify(values, null, 2));
+			//     props.setProject(values);
+			//     props.nextStep();
+			// }}
+			onSubmit={(values, actions) => {
+				setTimeout(() => {
+					alert(JSON.stringify(values, null, 2));
+					props.setProject(values);
+					props.nextStep();
+					actions.setSubmitting(false);
+				}, 1000);
+			}}
+		>
+			{(props) => (
+				<form onSubmit={props.handleSubmit}>
+					<div className={styles.section_input}>
+						<label
+							htmlFor="situation"
+							className={styles.section_question}
+						>
+							What is the problem space/goal of your project?
+							{` `}
+							What inspired you to start this project?
+							{` `}
+							Give a brief summary of your project's background.
+						</label>
+						<TaskDnD data={taskData} />
+						<Field
+							as="textarea"
+							className={`inp-field ${styles.input_situation}`}
+							name="situation"
+							placeholder="Type here"
+						/>
+					</div>
+					<Button
+						type="submit"
+						className={styles.save_draft}
+						iconR={<i className="fas fa-arrow-right" />}
+						text="Next"
+						onClick={props.submitForm}
+					/>
+				</form>
+			)}
+		</Formik>
+	);
 }
 export default withContext(tasksActions);
--- src/shared/reactDnD/task.js
+++ src/shared/reactDnD/task.js
@@ -1,112 +1,113 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
-import { Formik, Field, useField } from "formik";
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Formik, Field, useField } from 'formik';
 
-import DateSelect from "../../shared/DateSelect";
-import Popup from "../../shared/sandbox/popup";
-import Alert from "../sandbox/alert";
-import styles from "../../modules/DnD.module.scss";
+import DateSelect from '../../shared/DateSelect';
+import Popup from '../../shared/sandbox/popup';
+import Alert from '../sandbox/Alert';
+import styles from '../../modules/DnD.module.scss';
 
 const TaskCard = (props) => {
-    const [edit, setEdit] = useState(false);
-    // console.log(props.index)
-    const saveTask = (submit) => {
-        return (
-            <div className={styles.task_footer}>
-                <Popup
-                    BtnText="Delete Task"
-                    BtnColour="reddo"
-                    BtnId="delTask"
-                    BtnIconR={<i className="fas fa-trash-alt"></i>}
-                    contentBGColour={"white"}
-                    closeBtnLabel="No, go back"
-                    hasConfirm
-                    confirmBtnLabel="Yes, delete"
-                    onConfirm={props.deleteTask}
-                    width={500}
-                    content={<Alert id="delTask" type="task" />}
-                />
-                {/* <button onClick={() => props.deleteTask(props.index)}/> */}
-                <NavLink
-                    className={styles.edit_button}
-                    to="#"
-                    onClick={() => {setEdit(false); submit()}}
-                >
-                    <i className="fas fa-check"></i>
-                    <div className={styles.text_button}>Save</div>
-                </NavLink>
-            </div>
-        );
-    };
-    const editTask = () => {
-        return (
-            <div className={styles.task_footer}>
-                <DateSelect />
-                <NavLink
-                    className={styles.edit_button}
-                    to="#"
-                    onClick={() => setEdit(true)}
-                >
-                    <i className="fas fa-edit"></i>
-                    <div className={styles.text_button}>Edit</div>
-                </NavLink>
-            </div>
-        );
-    };
-    return (
-        <div className={styles.task_card}>
-            <div
-                className={`${styles.card} ${
-                    props.index === props.currentTask &&
-                    !props.snapshot.isDragging &&
-                    styles.task_current
-                } ${props.snapshot.isDragging && styles.task_dragging}`}
-            >
-                <Formik
-                    initialValues={{
-                        taskTitle: props.task.title,
-                        taskDescription: props.task.description,
-                    }}
-                    onSubmit={(values, actions) => {
-                        setTimeout(() => {
-                            alert(JSON.stringify(values, null, 2));
-                            // props.setProject(values);
-                            actions.setSubmitting(false);
-                        }, 1000);
-                    }}
-                >
-                    {(props) => (
-                        <form onSubmit={props.handleSubmit}>
-                            <Field
-                                as="input"
-                                name="taskTitle"
-                                className={styles.title}
-                                placeholder="New task title"
-                            />
-                            <Field
-                                as="textarea"
-                                name="taskDescription"
-                                className={styles.description_title}
-                                placeholder="Give this task a summary."
-                            />
-                            {edit ? saveTask(props.submitForm) : editTask()}
-                        </form>
-                    )}
-                    
-                </Formik>
-                <div className={styles.title}>
-                    Task {props.index + 1}: {props.task.title}
-                </div>
-                <div className={styles.description_title}>
-                    {props.task.description}
-                </div>
-            </div>
-            {props.index === props.currentTask && (
-                <div className={styles.task_chevron}>
-                    <i className="fas fa-chevron-left"></i>
-                </div>
-            )}
-        </div>
-    );
+	const [edit, setEdit] = useState(false);
+	const saveTask = (submit) => {
+		return (
+			<div className={styles.task_footer}>
+				<Popup
+					BtnText="Delete Task"
+					BtnColour="reddo"
+					BtnId="delTask"
+					BtnIconR={<i className="fas fa-trash-alt"></i>}
+					contentBGColour={'white'}
+					closeBtnLabel="No, go back"
+					hasConfirm
+					confirmBtnLabel="Yes, delete"
+					onConfirm={props.deleteTask}
+					width={500}
+					content={<Alert id="delTask" type="task" />}
+				/>
+				{/* <button onClick={() => props.deleteTask(props.index)}/> */}
+				<NavLink
+					className={styles.edit_button}
+					to="#"
+					onClick={() => {
+						setEdit(false);
+						submit();
+					}}
+				>
+					<i className="fas fa-check"></i>
+					<div className={styles.text_button}>Save</div>
+				</NavLink>
+			</div>
+		);
+	};
+	const editTask = () => {
+		return (
+			<div className={styles.task_footer}>
+				<DateSelect />
+				<NavLink
+					className={styles.edit_button}
+					to="#"
+					onClick={() => setEdit(true)}
+				>
+					<i className="fas fa-edit"></i>
+					<div className={styles.text_button}>Edit</div>
+				</NavLink>
+			</div>
+		);
+	};
+	return (
+		<div className={styles.task_card}>
+			<div
+				className={`${styles.card} ${
+					props.index === props.currentTask &&
+					!props.snapshot.isDragging &&
+					styles.task_current
+				} ${props.snapshot.isDragging && styles.task_dragging}`}
+			>
+				<Formik
+					initialValues={{
+						taskTitle: props.task.title,
+						taskDescription: props.task.description,
+					}}
+					onSubmit={(values, actions) => {
+						setTimeout(() => {
+							alert(JSON.stringify(values, null, 2));
+							// props.setProject(values);
+							actions.setSubmitting(false);
+						}, 1000);
+					}}
+				>
+					{(props) => (
+						<form onSubmit={props.handleSubmit}>
+							<Field
+								as="input"
+								name="taskTitle"
+								className={styles.title}
+								placeholder="New task title"
+							/>
+							<Field
+								as="textarea"
+								name="taskDescription"
+								className={styles.description_title}
+								placeholder="Give this task a summary."
+							/>
+							{edit ? saveTask(props.submitForm) : editTask()}
+						</form>
+					)}
+				</Formik>
+				<div className={styles.title}>
+					Task {props.index + 1}: {props.task.title}
+				</div>
+				<div className={styles.description_title}>
+					{props.task.description}
+				</div>
+			</div>
+			{props.index === props.currentTask && (
+				<div className={styles.task_chevron}>
+					<i className="fas fa-chevron-left"></i>
+				</div>
+			)}
+		</div>
+	);
 };
 export default TaskCard;
