abb692043924141856abb79a1816ebe80a4d255e
Angelinalim911 <61859285+Angelinalim911@users.noreply.github.com>
2020-11-07 22:56:15 +1100

    loading placeholder

--- src/pages/explore/explore.js
+++ src/pages/explore/explore.js
@@ -7,74 +7,84 @@ import styles from "../../modules/explore.module.scss";
 import SearchResult from "./SearchResult";
 import { getExploreUsers } from "../../shared/firebase/firebase";
 import Button from "../../shared/sandbox/Button";
+import Placeholder from "../../shared/sandbox/Placeholder";
 
 const getAllUsers = async (setUsers, limit, page) => {
-    const results = await getExploreUsers({ limit, page });
-    if (!results.data || results.data.length === 0) {
-        setUsers([]);
-    } else {
-        setUsers(results.data);
-    }
+	const results = await getExploreUsers({ limit, page });
+	if (!results.data || results.data.length === 0) {
+		setUsers([]);
+	} else {
+		setUsers(results.data);
+	}
 };
 
 const getFilterUsers = async (filter, setusers, limit, page) => {
-    console.log("getting filtered users");
+	console.log("getting filtered users");
 };
 
 const Explore = (props) => {
-    const [users, setUsers] = useState([]);
-    const [page, setPage] = useState(1);
-    const [limit, setLimit] = useState(2);
-    useEffect(() => {
-        setPage(1);
-        setLimit(2);
-    }, []);
-    useEffect(() => {
-        getAllUsers(setUsers, limit, page);
-    }, [page]);
-    return (
-        <div>
-            <Button
-                text="pageup"
-                onClick={() => {
-                    setPage(page + 1);
-                }}
-            />
-            Page {page}
-            <Tab />
-            <div className={styles.container}>
-                <div className={styles.filter}>
-                    <strong>Sort by:</strong>
-                    <br />
-                    <Checkbox id="1" label="Most endorsed" />
-                    <Checkbox id="2" label="Most recent" />
-                    <Checkbox id="3" label="Most relevant" />
-                    <Checkbox id="4" label="Most projects" />
-                    <Checkbox id="5" label="Most experienced" />
-                </div>
-                <div className={styles.results}>
-                    <h6 className={styles.h6}>
-                        Search results ({users.length})
-                    </h6>
-                    {users.length === 0 && (
-                        <h3>Loading...(make this pretty potential)</h3>
-                    )}
-                    {users.map((user, index) => {
-                        return (
-                            <SearchResult
-                                key={index}
-                                uid={user.uid}
-                                name={`${user.firstName} ${user.lastName}`}
-                                location={user.location}
-                                occupation={user.occupation}
-                                photoUrl={user.photo}
-                            />
-                        );
-                    })}
-                </div>
-            </div>
-        </div>
-    );
+	const [users, setUsers] = useState([]);
+	const [page, setPage] = useState(1);
+	const [limit, setLimit] = useState(2);
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		setTimeout(() => setLoading(false), 6000)
+	  }, [])
+	useEffect(() => {
+		setPage(1);
+		setLimit(2);
+	}, []);
+	useEffect(() => {
+		getAllUsers(setUsers, limit, page);
+	}, [page]);
+	return (
+		<div>
+			<Button
+				text='pageup'
+				onClick={() => {
+					setPage(page + 1);
+				}}
+			/>
+			Page {page}
+			<Tab />
+			<div className={styles.container}>
+				<div className={styles.filter}>
+					<strong>Sort by:</strong>
+					<br />
+					<Checkbox id='1' label='Most endorsed' />
+					<Checkbox id='2' label='Most recent' />
+					<Checkbox id='3' label='Most relevant' />
+					<Checkbox id='4' label='Most projects' />
+					<Checkbox id='5' label='Most experienced' />
+				</div>
+				<div className={styles.results}>
+					<h6 className={styles.h6}>Search results ({users.length})</h6>
+					{loading ? (
+						<>
+							<Placeholder />
+							<Placeholder />
+							<Placeholder />
+						</>
+					) : (
+						<div>
+							{users.map((user, index) => {
+								return (
+									<SearchResult
+										key={index}
+										uid={user.uid}
+										name={`${user.firstName} ${user.lastName}`}
+										location={user.location}
+										occupation={user.occupation}
+										photoUrl={user.photo}
+									/>
+								);
+							})}
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Explore;
