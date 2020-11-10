0e61fd0115f1a1c4aacbf3e8a7363cf19841d542
Khairi Johari <mksjohari@gmail.com>
2020-11-11 06:06:58 +1100

    trasnfer

--- src/shared/firebase/firebase.js
+++ src/shared/firebase/firebase.js
@@ -1,48 +1,68 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/functions';
-import 'firebase/firestore';
-import { getFirebase, getFunctions, getStorage } from './config';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/functions";
+import "firebase/firestore";
+import { getFirebase, getFunctions, getStorage } from "./config";
 
 // FUNCTIONS //
 
 export const createAccount = getFunctions(firebase).httpsCallable(
-	'createAccount'
+    "createAccount"
 );
 
 export const checkUserExists = getFunctions(firebase).httpsCallable(
-	'checkUserExists'
+    "checkUserExists"
 );
 
-export const getUserInfo = getFunctions(firebase).httpsCallable('getUserInfo');
+export const getUserInfo = getFunctions(firebase).httpsCallable("getUserInfo");
 export const getPublicInfo = getFunctions(firebase).httpsCallable(
-	'getPublicInfo'
+    "getPublicInfo"
 );
 export const getUserSocials = getFunctions(firebase).httpsCallable(
-	'getUserSocials'
+    "getUserSocials"
 );
 export const getUserExpertise = getFunctions(firebase).httpsCallable(
-	'getUserExpertise'
+    "getUserExpertise"
 );
 
 export const finishUserSignUp = getFunctions(firebase).httpsCallable(
-	'finishUserSignUp'
+    "finishUserSignUp"
 );
 
 export const getExploreUsers = getFunctions(firebase).httpsCallable(
-	'getExploreUsers'
+    "getExploreUsers"
 );
 
 // FIRESTORE
 
 export const uploadProject = async (uuid, project) => {
-	await getFirebase()
-		.firestore()
-		.collection('users')
-		.doc(uuid)
-		.collection('projects')
-		.doc(project.projectId)
-		.set(project);
+    const projectBase = {
+        pid: project.projectId,
+        title: project.title,
+        visibility: project.sharing,
+        situation: {
+            summary: project.situation.summary,
+            role: project.situation.role,
+            teamSize: project.situation.teamSize,
+            currency: project.situation.currency,
+            budget: project.situation.budget,
+            startDate: new Date(
+                project.situation.projectDates.startDate.format()
+            ),
+            endDate: new Date(project.situation.projectDates.endDate.format()),
+        },
+        results: {
+            conclusion: project.results.conclusion,
+            links: project.results.links,
+        },
+    };
+    await getFirebase()
+        .firestore()
+        .collection("users")
+        .doc(uuid)
+        .collection("projects")
+        .doc(project.projectId)
+        .set(projectBase);
 };
 
 // STORAGE //
