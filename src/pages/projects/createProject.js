879061b58a71b15caa8a334ca3028b2ab49e25e4
mksjohari <mksjohari@gmail.com>
2020-11-11 06:18:01 +1100

    merge

--- src/pages/projects/createProject.js
+++ src/pages/projects/createProject.js
@@ -1,170 +1,170 @@
-import React, { useState } from "react";
-import { connect } from "react-redux";
-import { withFormik } from "formik";
-import { withContext } from "../../shared/react-dims";
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { withFormik } from 'formik';
+import { withContext } from '../../shared/react-dims';
 
-import Button from "../../shared/sandbox/Button";
-import Timeline from "../../shared/sandbox/Timeline";
-import { lockBg } from "../../shared/sandbox/Popup";
-import ProjectDetails from "../../shared/input/ProjectDetails";
-import Situation from "./situation";
-import TasksActions from "./tasksActions";
-import Results from "./results";
+import Button from '../../shared/sandbox/Button';
+import Timeline from '../../shared/sandbox/Timeline';
+import { lockBg } from '../../shared/sandbox/Popup';
+import ProjectDetails from '../../shared/input/ProjectDetails';
+import Situation from './situation';
+import TasksActions from './tasksActions';
+import Results from './results';
 
-import popup from "../../modules/popup.module.scss";
-import styles from "../../modules/createProject.module.scss";
-import DevButton from "../../shared/sandbox/devButton";
-import { uploadProject } from "../../shared/firebase/firebase";
-import { useHistory } from "react-router-dom";
+import popup from '../../modules/popup.module.scss';
+import styles from '../../modules/createProject.module.scss';
+import DevButton from '../../shared/sandbox/devButton';
+import { uploadProject } from '../../shared/firebase/firebase';
+import { useHistory } from 'react-router-dom';
 
 function mapStateToProps(state) {
-    return { user: state.user };
+	return { user: state.user };
 }
 
 function CreateProject(props) {
-    const [percent, setPercent] = useState(0);
-    const [step, setStep] = useState(0);
-    const [project, setProject] = useState(projectData);
-    const history = useHistory();
-    function nextStep(props) {
-        if (step < 3) {
-            setPercent((step * 100 + 100) / 3);
-            setStep(step + 1);
-        }
-    }
-    function prevStep() {
-        if (step > 0) {
-            setPercent((step * 100 - 100) / 3);
-            setStep(step - 1);
-        }
-    }
-    function addSection(values) {
-        const newSections = [...project.results.sections, values];
-        const newProject = { ...project };
-        newProject.results.sections = newSections;
-        setProject(newProject);
-    }
-    function editProjectDetails(values) {
-        const newProject = { ...project };
-        newProject.status = values.status;
-        newProject.sharing = values.sharing;
-        newProject.title = values.title;
-        setProject(newProject);
-    }
-    function editSituation(values) {
-        const newProject = { ...project };
-        newProject.situation = values;
-        setProject(newProject);
-    }
-    function editTasks(values) {
-        const newProject = { ...project };
-        newProject.tasks = values;
-        setProject(newProject);
-    }
-    function editResults(values) {
-        const newProject = { ...project };
-        newProject.results = values;
-        setProject(newProject);
-    }
-    function editSections(index, values) {
-        const newSections = [...project.results.sections];
-        newSections[index].description = values.description;
-        newSections[index].sectionLink = values.sectionLink;
-        newSections[index].files = values.files;
-        const newProject = { ...project };
-        newProject.results.sections[index] = newSections;
-        setProject(newProject);
-    }
-    const uploadToFirestore = async () => {
-        console.log(project);
-        console.log("uploading to firestore");
-        uploadProject(props.user.uuid, project);
-        history.push(`users/${props.user.uid}`);
-    };
-    return (
-        <div className={styles.root}>
-            <div className={styles.header}>
-                <div className={styles.button_row}>
-                    <Button
-                        type="submit"
-                        className={styles.save_draft}
-                        iconR={<i className="far fa-save"></i>}
-                        text="Save draft"
-                    />
-                    <Button
-                        className={styles.delete_project}
-                        iconR={<i className="far fa-trash-alt"></i>}
-                        text="Delete project"
-                    />
-                </div>
-            </div>
-            <div
-                id={project.projectId}
-                className={styles.project_title}
-                onClick={(e) => {
-                    lockBg(e);
-                }}
-            >
-                {project.title}
-            </div>
-            <div
-                id={project.projectId + "_popContent"}
-                className={popup.popupContainer}
-            >
-                <ProjectDetails
-                    id={project.projectId}
-                    project={project}
-                    editProjectDetails={editProjectDetails}
-                />
-            </div>
-            <div className={styles.title_help}>
-                Click on the title to edit project details.
-            </div>
-            <div className={styles.section}>
-                <Timeline
-                    label={label}
-                    percent={percent}
-                    width={props.dims.width * 0.7}
-                />
-            </div>
+	const [percent, setPercent] = useState(0);
+	const [step, setStep] = useState(0);
+	const [project, setProject] = useState(projectData);
+	const history = useHistory();
+	function nextStep(props) {
+		if (step < 3) {
+			setPercent((step * 100 + 100) / 3);
+			setStep(step + 1);
+		}
+	}
+	function prevStep() {
+		if (step > 0) {
+			setPercent((step * 100 - 100) / 3);
+			setStep(step - 1);
+		}
+	}
+	function addSection(values) {
+		const newSections = [...project.results.sections, values];
+		const newProject = { ...project };
+		newProject.results.sections = newSections;
+		setProject(newProject);
+	}
+	function editProjectDetails(values) {
+		const newProject = { ...project };
+		newProject.status = values.status;
+		newProject.sharing = values.sharing;
+		newProject.title = values.title;
+		setProject(newProject);
+	}
+	function editSituation(values) {
+		const newProject = { ...project };
+		newProject.situation = values;
+		setProject(newProject);
+	}
+	function editTasks(values) {
+		const newProject = { ...project };
+		newProject.tasks = values;
+		setProject(newProject);
+	}
+	function editResults(values) {
+		const newProject = { ...project };
+		newProject.results = values;
+		setProject(newProject);
+	}
+	function editSections(index, values) {
+		const newSections = [...project.results.sections];
+		newSections[index].description = values.description;
+		newSections[index].sectionLink = values.sectionLink;
+		newSections[index].files = values.files;
+		const newProject = { ...project };
+		newProject.results.sections[index] = newSections;
+		setProject(newProject);
+	}
+	const uploadToFirestore = async () => {
+		console.log(project);
+		console.log('uploading to firestore');
+		uploadProject(props.user.uuid, project);
+		history.push(`users/${props.user.uid}`);
+	};
+	return (
+		<div className={styles.root}>
+			<div className={styles.header}>
+				<div className={styles.button_row}>
+					<Button
+						type="submit"
+						className={styles.save_draft}
+						iconR={<i className="far fa-save"></i>}
+						text="Save draft"
+					/>
+					<Button
+						className={styles.delete_project}
+						iconR={<i className="far fa-trash-alt"></i>}
+						text="Delete project"
+					/>
+				</div>
+			</div>
+			<div
+				id={project.projectId}
+				className={styles.project_title}
+				onClick={(e) => {
+					lockBg(e);
+				}}
+			>
+				{project.title}
+			</div>
+			<div
+				id={project.projectId + '_popContent'}
+				className={popup.popupContainer}
+			>
+				<ProjectDetails
+					id={project.projectId}
+					project={project}
+					editProjectDetails={editProjectDetails}
+				/>
+			</div>
+			<div className={styles.title_help}>
+				Click on the title to edit project details.
+			</div>
+			<div className={styles.section}>
+				<Timeline
+					label={label}
+					percent={percent}
+					width={props.dims.width * 0.7}
+				/>
+			</div>
 
-            {step === 0 && (
-                <div className={styles.parent_form}>
-                    <div className={styles.heading}>Situation</div>
-                    <Situation
-                        situation={project.situation}
-                        nextStep={nextStep}
-                        prevStep={prevStep}
-                        editSituation={editSituation}
-                    />
-                </div>
-            )}
-            {step === 1 && (
-                <div className={styles.parent_form}>
-                    <div className={styles.heading}>Tasks & Actions</div>
-                    <TasksActions
-                        tasks={project.tasks}
-                        nextStep={nextStep}
-                        prevStep={prevStep}
-                        editTasks={editTasks}
-                    />
-                </div>
-            )}
-            {step === 2 && (
-                <div className={styles.parent_form}>
-                    <div className={styles.heading}>Results</div>
-                    <Results
-                        results={project.results}
-                        nextStep={nextStep}
-                        prevStep={prevStep}
-                        editResults={editResults}
-                        addSection={addSection}
-                        editSections={editSections}
-                        finishProject={uploadToFirestore}
-                    />
-                </div>
-            )}
-            {/* {step === 3 && <TempPreview project={project} />}
+			{step === 0 && (
+				<div className={styles.parent_form}>
+					<div className={styles.heading}>Situation</div>
+					<Situation
+						situation={project.situation}
+						nextStep={nextStep}
+						prevStep={prevStep}
+						editSituation={editSituation}
+					/>
+				</div>
+			)}
+			{step === 1 && (
+				<div className={styles.parent_form}>
+					<div className={styles.heading}>Tasks & Actions</div>
+					<TasksActions
+						tasks={project.tasks}
+						nextStep={nextStep}
+						prevStep={prevStep}
+						editTasks={editTasks}
+					/>
+				</div>
+			)}
+			{step === 2 && (
+				<div className={styles.parent_form}>
+					<div className={styles.heading}>Results</div>
+					<Results
+						results={project.results}
+						nextStep={nextStep}
+						prevStep={prevStep}
+						editResults={editResults}
+						addSection={addSection}
+						editSections={editSections}
+						finishProject={uploadToFirestore}
+					/>
+				</div>
+			)}
+			{/* {step === 3 && <TempPreview project={project} />}
 			<div className={styles.button_back}>
 				{step !== 0 && (
 					<Button
@@ -174,89 +174,89 @@ function CreateProject(props) {
 					/>
 				)}
 			</div> */}
-        </div>
-    );
+		</div>
+	);
 }
 
-const label = ["SITUATION", "TASKS & ACTIONS", "RESULTS", "PREVIEW"];
+const label = ['SITUATION', 'TASKS & ACTIONS', 'RESULTS', 'PREVIEW'];
 
 const ProjectForm = withFormik({
-    mapPropsToValues: () => ({ name: "" }),
+	mapPropsToValues: () => ({ name: '' }),
 
-    // Custom sync validation
-    validate: (values) => {
-        const errors = {};
+	// Custom sync validation
+	validate: (values) => {
+		const errors = {};
 
-        if (!values.name) {
-            errors.name = "Required";
-        }
+		if (!values.name) {
+			errors.name = 'Required';
+		}
 
-        return errors;
-    },
+		return errors;
+	},
 
-    handleSubmit: (values, { setSubmitting }) => {
-        console.log("submititng");
-        setTimeout(() => {
-            setSubmitting(false);
-        }, 1000);
-    },
+	handleSubmit: (values, { setSubmitting }) => {
+		console.log('submititng');
+		setTimeout(() => {
+			setSubmitting(false);
+		}, 1000);
+	},
 
-    displayName: "ProjectForm",
+	displayName: 'ProjectForm',
 })(CreateProject);
 
 export default connect(mapStateToProps)(withContext(ProjectForm));
 
 const projectData = {
-    projectId: `project-${Math.floor(Math.random() * Math.pow(10, 13))}`,
-    status: "Ongoing",
-    sharing: "Public",
-    title: "New project",
-    situation: {
-        summary: "",
-        role: "",
-        teamSize: "1",
-        budget: 0,
-        currency: "AUD",
-        projectDates: { startDate: null, endDate: null },
-    },
-    tasks: [
-        {
-            taskId: `task-${new Date().getTime()}`,
-            title: "New task",
-            description: "",
-            startDate: null,
-            endDate: null,
-            actions: [
-                // {
-                //     actionId: `action-${new Date().getTime()}`,
-                //     title: "New action",
-                //     tools: [],
-                //     skills: [],
-                //     description: "",
-                //     files: [],
-                // },
-            ],
-        },
-    ],
-    results: {
-        conclusion: "",
-        links: [
-            // {
-            //     url: "",
-            //     linkName: "",
-            // },
-        ],
-        sections: [
-            // {
-            //     sectionId: `section-${new Date().getTime()}`,
-            //     description:
-            //         "Longer description ayyyyyyy, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse",
-            //     files: [],
-            //     sectionLink: {
-            //         url: "",
-            //         linkName: "",
-            //     },
-            // },
-        ],
-    },
+	projectId: `project-${Math.floor(Math.random() * Math.pow(10, 13))}`,
+	status: 'Ongoing',
+	sharing: 'Public',
+	title: 'New project',
+	situation: {
+		summary: '',
+		role: '',
+		teamSize: '1',
+		budget: 0,
+		currency: 'AUD',
+		projectDates: { startDate: null, endDate: null },
+	},
+	tasks: [
+		{
+			taskId: `task-${new Date().getTime()}`,
+			title: 'New task',
+			description: '',
+			startDate: null,
+			endDate: null,
+			actions: [
+				// {
+				//     actionId: `action-${new Date().getTime()}`,
+				//     title: "New action",
+				//     tools: [],
+				//     skills: [],
+				//     description: "",
+				//     files: [],
+				// },
+			],
+		},
+	],
+	results: {
+		conclusion: '',
+		links: [
+			// {
+			//     url: "",
+			//     linkName: "",
+			// },
+		],
+		sections: [
+			// {
+			//     sectionId: `section-${new Date().getTime()}`,
+			//     description:
+			//         "Longer description ayyyyyyy, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse",
+			//     files: [],
+			//     sectionLink: {
+			//         url: "",
+			//         linkName: "",
+			//     },
+			// },
+		],
+	},
 };
