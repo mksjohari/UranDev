1f0e6fa51f4f9306ab7c8b5c037b32be0cb3aacb
cheang-haoyuan <cheangh1@student.unimelb.edu.au>
2020-11-16 00:27:25 +1100

    pdf preview full scroll and page version

--- src/pages/tmp/tmpTest.js
+++ src/pages/tmp/tmpTest.js
@@ -5,32 +5,66 @@ import * as moment from 'moment';
 import TaskDnD from '../../shared/reactDnD/taskDnD';
 import GanttChart from '../../shared/GanttChart';
 import PDFPreview from '../../shared/sandbox/PDFPreview';
+import Button from '../../shared/sandbox/Button';
+
 
 const TmpTest = (props) => {
 
-	const [PDF, setPDF] = useState({});
+	const [PDF, setPDF] = useState("");
+	const [imageSection, setImageSection] = useState(false);
+	const [PDFSection, setPDFSection] = useState(false);
+	const handleClose = (str) => (str === 'pdf' ? setPDFSection(false) : setImageSection(false));
+	const handleShow = (str) => (str === 'pdf' ? setPDFSection(true) : setImageSection(true));
 
+	const showImages = () => {
+		handleClose('pdf');
+		handleShow('images');
+	};
+	const showPDFs = () => {
+		handleClose('images');
+		handleShow('pdf');
+	};
+
+	function handleChange(event) {
+		setPDF(
+			event.target.files[0]
+			);
+		console.log(PDF);
+	}
+
+	
 	function sampleChartFunc(id) {
 		console.log(id);
 	}
 	
 	console.log('tmp props', props);
 
-	function handleChange(event) {
-		setPDF(
-			event.target.files[0]
-			);
-		console.log(setPDF);
-	}
-
-
 
 	return (
 		<div className="tmp">
-			<div className={""}>
-						<label
-							className={` `}
-						>
+			<div>
+				<p>What are you uploading?</p>
+				<div>
+					(for Michelle) this is just an example, I think that Segment tab you did would work well for this :)
+				</div>
+				<Button type='button' text='Images' onClick={showImages} />
+				<Button type='button' text="PDF's" onClick={showPDFs} />
+			</div>
+			{imageSection ? 
+				(
+				<div>
+					<p>
+						Image result section
+					</p>
+				</div>
+				)
+				: ("") 
+			}
+			{PDFSection ? 
+				(<>
+				<p>Please select ONE pdf file</p>
+					<div className={""} style={{width: '100px', border:'solid 1px', borderRadius: '15px' }} >
+						<label>
 							<input type="file" accept="pdf" onChange={handleChange} />
 							<i
 								className="fas fa-file"
@@ -38,11 +72,12 @@ const TmpTest = (props) => {
 							/>
 							Upload PDF
 						</label>
-						<p className={""}>
-						Upload only ONE .pdf file
-				</p>
-			</div>
-			<PDFPreview setFile={setPDF} file={PDF} />
+					</div>
+					<PDFPreview setFile={setPDF} file={PDF} />
+				</>)
+
+			: ("")
+			}
 			<GanttChart data={projectData} chartEvent={sampleChartFunc} />
 			{/* 
 			<Popup
