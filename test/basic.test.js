5ac85e42ee168d9dea85f05bc1456448f4d5ccac
Angelinalim911 <61859285+Angelinalim911@users.noreply.github.com>
2020-11-20 00:05:48 +0800

    sample frontend test

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,15 +1,52 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
-import Basic from "./basic";
-import { configure } from "enzyme";
+import ReactDOM from "react-dom";
+import { shallow, configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+const renderer = require("react-test-renderer");
 configure({ adapter: new Adapter() });
 
-it("renders without crashing", () => {
-  shallow(<Basic />);
+import Basic from "./basic";
+import SignUp from "../src/shared/signUpForm";
+// import App from "../src/App";
+
+let container = null;
+beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
 });
 
-// it("renders correctly", () => {
-//   const wrapper = mount(<Basic />);
-//   expect(wrapper.state("error")).toEqual(null);
-// });
+afterEach(() => {
+    // cleanup on exiting
+    ReactDOM.unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+test("example component renders without crashing", () => {
+    shallow(<Basic />);
+});
+
+test("component matches previous snapshots", () => {
+    expect(renderer.create(<Basic />)).toMatchSnapshot();
+});
+
+test("example component renders correctly", () => {
+    ReactDOM.render(<Basic />, container);
+    expect(document.querySelector("h1").innerHTML).toBe("Basic Test");
+});
+
+// const wrapper = mount(<Basic />);
+// expect(wrapper.state("error")).toEqual(null);
+
+// const wrapper = shallow(<SignUp />).dive();
+
+// jest.mock('react-router-dom', () => ({
+//   useLocation: jest.fn().mockReturnValue({
+//     pathname: '/another-route',
+//     search: '',
+//     hash: '',
+//     state: null,
+//     key: '5nvxpbdafa',
+//   }),
+// }));
